eb53aacdf5706ce9c58ffe04e012a0730c47d010
danieljbell <djbell70@gmail.com>
2017-07-24 11:06:13 -0400

    conditional for removing marketo CSS

--- templates/components/scripts/_removeMarketoCSS.js
+++ templates/components/scripts/_removeMarketoCSS.js
@@ -6,6 +6,8 @@
         var removeNodes = [];
         removeNodes.push(document.querySelector('#mktoForms2BaseStyle'));
         removeNodes.push(document.querySelector('#mktoForms2ThemeStyle'));
-        for (var i = 0; i < removeNodes.length; i++) {
-          removeNodes[i].remove();
+        if (removeNodes) {
+            for (var i = 0; i < removeNodes.length; i++) {
+              removeNodes[i].remove();
+            }
         }
