7edaf19d0fc95e6355bf042833a6c92098828e14
danieljbell <djbell70@gmail.com>
2017-07-24 11:45:41 -0400

    commenting out styles

--- templates/components/scripts/_removeMarketoCSS.js
+++ templates/components/scripts/_removeMarketoCSS.js
@@ -3,11 +3,11 @@
         REMOVE MARKETO CSS
         ==============================
         */
-        var removeNodes = [];
-        removeNodes.push(document.querySelector('#mktoForms2BaseStyle'));
-        removeNodes.push(document.querySelector('#mktoForms2ThemeStyle'));
-        if (removeNodes.length) {
-            for (var i = 0; i < removeNodes.length; i++) {
-              removeNodes[i].remove();
-            }
-        }
+        // var removeNodes = [];
+        // removeNodes.push(document.querySelector('#mktoForms2BaseStyle'));
+        // removeNodes.push(document.querySelector('#mktoForms2ThemeStyle'));
+        // if (removeNodes.length > 1) {
+        //     for (var i = 0; i < removeNodes.length; i++) {
+        //       removeNodes[i].remove();
+        //     }
+        // }
